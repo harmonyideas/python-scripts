33937c9d6df7942aec148c24e16cf6cd3860c586
Mike <mccs.mike@gmail.com>
2018-01-17 16:56:47 -0500

    Update bubblesort.py

--- learning-python/bubblesort.py
+++ learning-python/bubblesort.py
@@ -4,11 +4,11 @@ import random
 def bubble_sort(a_list):
     for passnum in range(len(a_list)-1, 0, -1):
         for i in range(passnum):
-            if (a_list[i] > a_list[i + 1]):
-                temp = a_list[i]
-                a_list[i] = a_list[i + 1]
-                a_list[i + 1] = temp
+            if  a_list[i] > a_list[i + 1]:
+                a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
 
+    return a_list
 
 a_list = random.sample(range(100), 100)
+
 bubble_sort(a_list)

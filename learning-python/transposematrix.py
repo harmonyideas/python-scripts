af386f82e723ea43c18a5a5cf165f6013daf8f7f
Mike <mccs.mike@gmail.com>
2018-01-17 17:04:10 -0500

    Update transposematrix.py

--- learning-python/transposematrix.py
+++ learning-python/transposematrix.py
@@ -1,9 +1,6 @@
 def transpose_matrix(matrix):
     for i in xrange(len(matrix)):
         for j in xrange(i, len(matrix)):
-            temp = matrix[i][j]
-            matrix[i][j] = matrix[j][i]
-            matrix[j][i] = temp
-
-
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            
 transpose_matrix([[1, 0, 1], [1, 0, 1], [1, 0, 1]])

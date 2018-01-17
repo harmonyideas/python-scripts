f0b9917b8c9690fc8d5048dbfdf766f4ffa82bc2
Mike <mccs.mike@gmail.com>
2018-01-17 16:57:54 -0500

    Update bubblesortanimation.py

--- learning-python/bubblesortanimation.py
+++ learning-python/bubblesortanimation.py
@@ -9,14 +9,12 @@ ax = plt.bar(y_pos, a_list, align='center', color='g', edgecolor='w')
 plt.title('Bubble Sort Algorithm')
 
 for passnum in range(len(a_list), 0, -1):
-    plt.pause(.1)
+    plt.pause(.01)
     for i in range(passnum - 1):
         if a_list[i] > a_list[i + 1]:
             ax[i].set_height(a_list[i + 1])
-            temp = a_list[i]
-            a_list[i] = a_list[i + 1]
-            a_list[i + 1] = temp
-            ax[i + 1].set_height(temp)
+            ax[i + 1].set_height(a_list[i])
+            a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
 
 # Window will close without this - need to fix and update individual bars instead
 plt.show()

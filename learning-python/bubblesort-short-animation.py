06ab4030c761211559406ec1e7b2c9ad234d1e29
Mike <mccs.mike@gmail.com>
2018-01-19 15:38:25 -0500

    Update bubblesort-short-animation.py

--- learning-python/bubblesort-short-animation.py
+++ learning-python/bubblesort-short-animation.py
@@ -2,23 +2,25 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 
-a_list = random.sample(range(50), 50)
-y_pos = np.arange(len(a_list))
-plt.title('Bubble Sort (Short) Algorithm')
-ax = plt.bar(y_pos, a_list, align='center', color='g', edgecolor='w')
+x_values = random.sample(range(25), 25)
+y_values = np.arange(len(x_values))
+plt.title('Bubble Sort Algorithm')
+ax = plt.bar(y_values, x_values, align='center', color='g', edgecolor='w')
 
 swap = True
-numpasses = len(a_list) - 1
+numpasses = len(x_values) - 1
 while numpasses > 0 and swap:
     swap = False
     # Adjust individual bars to proper height of a_list[index]
     for i in range(numpasses):
-        plt.pause(0.05)
-        if a_list[i] > a_list[i + 1]:
+        plt.pause(0.1)
+        if x_values[i] > x_values[i + 1]:
             swap = True
-            ax[i].set_height(a_list[i + 1])
-            ax[i + 1].set_height(a_list[i])
-            a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
+            temp = x_values[i]
+            ax[i].set_height(x_values[i + 1])
+            x_values[i] = x_values[i + 1]
+            x_values[i + 1] = temp
+            ax[i + 1].set_height(temp)
     numpasses = numpasses-1
 
 # Window will close without this

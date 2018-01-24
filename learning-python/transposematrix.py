dbf018df9a545dbbcc20a45b441ef28dbf4cbcff
mike <mike@harmonyideas.com>
2018-01-24 11:25:19 -0500

    Return matrix and print results

--- learning-python/transposematrix.py
+++ learning-python/transposematrix.py
@@ -2,5 +2,6 @@ def transpose_matrix(matrix):
     for i in xrange(len(matrix)):
         for j in xrange(i, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    return matrix
             
-transpose_matrix([[1, 0, 1], [1, 0, 1], [1, 0, 1]])
+print(transpose_matrix([[1, 0, 1], [1, 0, 1], [1, 0, 1]]))

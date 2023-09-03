e916dcd0b1f140c691e528a92c9cf75cc7acd9b0
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:14 -0400

    Optimize

--- learning-python/deckofcards.py
+++ learning-python/deckofcards.py
@@ -1,34 +1,32 @@
 import random
-import itertools
-
 
 class MyCards(object):
+
+    VALUES = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 
     def __init__(self):
         self.deck = []
         self.build()
 
     def build(self):
-        self.deck = list(itertools.product(['A','2','3','4','5','6','7',
-        '8','9','10','J','Q','K'],['Clubs','Diamonds','Hearts','Spades']))
+        self.deck = [(value, suit) for value in self.VALUES for suit in self.SUITS]
 
     def join(self, iterable, sep=' '):
         return sep.join(map(str, iterable))
 
     def card_combinations(self, cards, n):
-        return {self.join(combos)
-                for combos in itertools.combinations(cards, n)}
+        return {self.join(combos) for combos in itertools.combinations(cards, n)}
 
-    def pick_random_cards(self, counter):
-        return random.sample(self.deck, abs(counter))
+    def pick_random_cards(self, count):
+        return random.sample(self.deck, abs(count))
 
     def shuffle_cards(self, repeat=1):
-        for x in range(0, abs(repeat)):
+        for x in range(abs(repeat)):
             random.shuffle(self.deck)
 
     def print_cards(self):
         print(self.deck)
-
 
 cards = MyCards()
 cards.shuffle_cards(1)
@@ -37,4 +35,4 @@ cards.shuffle_cards(1)
 # print(cards.card_combinations(cards.deck, 2))
 
 # 4 random cards
-#print(cards.pick_random_cards(4))
+# print(cards.pick_random_cards(4))

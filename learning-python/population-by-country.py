9fc67f9197570823df3974bb52c0c0befb517d54
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:12 -0400

    Update population-by-country.py

--- learning-python/population-by-country.py
+++ learning-python/population-by-country.py
@@ -1,25 +1,21 @@
 import csv
 import matplotlib.pyplot as plt
 
-y_values = []
-x_values = list(range(1980, 2011))
-x_values.insert(0, 'country')
-
-# Should be using the pandas library for this - will update in future
 with open('populationbycountry19802010millions.csv', 'rU') as csvfile:
-    fieldnames = x_values
-    stats = csv.DictReader(csvfile, delimiter=',', quotechar='"', fieldnames=fieldnames)
-
+    stats = csv.DictReader(csvfile, delimiter=',', quotechar='"')
     for row in stats:
         if row['country'] == 'North America':
-            for v in x_values:
-                y_values.append(row[(v)])
+            x_values = sorted(row.keys())
+            y_values = sorted(row.values())
 
 plt.style.use(['ggplot'])
-ax = plt.plot(x_values[1:], y_values[1:])
+ax = plt.plot(x_values[:-1], y_values[:-1])
 plt.title('Population of North America')
 plt.ylabel('Population')
 plt.xlabel('Year')
 plt.show()
+
+
+
 
 

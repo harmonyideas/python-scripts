cfd689e87e35c8cd3a032b549ef1ddd181664c85
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:13 -0400

    Remove plt.pause
    Update figure instead of pausing

--- learning-python/bubblesort-short-animation.py
+++ learning-python/bubblesort-short-animation.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 
+fig = plt.gcf()
+fig.show()
+
 x_values = random.sample(range(25), 25)
 y_values = np.arange(len(x_values))
 plt.title('Bubble Sort Algorithm')
@@ -11,16 +14,15 @@ swap = True
 numpasses = len(x_values) - 1
 while numpasses > 0 and swap:
     swap = False
-    # Adjust individual graph bars to proper height 
+    # Adjust individual graph bars to proper height
     for i in range(numpasses):
-        plt.pause(0.1)
+        fig.canvas.draw()
         if x_values[i] > x_values[i + 1]:
             swap = True
             ax[i].set_height(x_values[i + 1])
             ax[i + 1].set_height(x_values[i])
             x_values[i], x_values[i + 1] = x_values[i + 1], x_values[i]
-    numpasses = numpasses-1
+    numpasses = numpasses - 1
 
 # Window will close without this
 plt.show()
-

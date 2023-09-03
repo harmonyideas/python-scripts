461f1f5fda189677ac33b913bb7d207ea6949633
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:13 -0400

    Create multiplication-table.py

---
+++ learning-python/multiplication-table.py
@@ -0,0 +1,21 @@
+def multiplication_table():
+
+    colheaders = ['1', '2', '3', '4', '5',
+                  '6', '7', '8', '9', '10', '11', '12']
+    rowheaders = ['1', '2', '3', '4', '5', '6',
+                  '7', '8', '9', '10', '11', '12', '13']
+
+    rows = range(1, 13)
+    cols = range(1, 13)
+
+    for c in colheaders:
+        print("\t" + "[" + c + "]"),
+
+    for r in rows:
+        print("\n" + "[" + rowheaders[r - 1] + "]"),
+        for c in cols:
+            print ("\t" + str(r * c)),
+        print ("\n")
+
+
+multiplication_table()

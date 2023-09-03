8c18123bd2a4c8646882dce48195165004cf5fc3
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:14 -0400

    Update squares.py

--- learning-python/squares.py
+++ learning-python/squares.py
@@ -2,7 +2,7 @@ import random
 
 def square_list(my_list):
     squared_list = [ i**2 for i in my_list ]
-    print squared_list
+    print(squared_list)
 
 numbers = list(xrange(0,25))
 square_list(numbers)

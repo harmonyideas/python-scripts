c0e99f3323240217db1f5472fd4fcc2fcd3456d5
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:11 -0400

    Update deckofcards.py

--- learning-python/deckofcards.py
+++ learning-python/deckofcards.py
@@ -3,7 +3,7 @@ import random
 
 class Cards(object):
     def __init__(self, counter=0):
-        self.value = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+        self.value = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
         self.rank = ['c', 'd', 'h', 's']
         Cards.deck = range(52)
         Cards.deck = [(A + B) for A in self.value for B in self.rank]

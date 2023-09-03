05a6399052611d196b86c9427ecbaedfb9d08bbc
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:09 -0400

    Create probability.py

---
+++ probability.py
@@ -0,0 +1,33 @@
+from math import factorial
+import random
+import itertools
+
+
+def choices(x, y):
+    return factorial(x) // (factorial(x - y) * factorial(y))
+
+
+def combine(a, b):
+    """Concatenate"""
+    return {A + B
+            for A in a for B in b}
+
+
+def join(iterable, sep=' '):
+    """Join the items in iterable, mapping each to a string first."""
+    return sep.join(map(str, iterable))
+
+
+def combo(items, n):
+        return {join(combos)
+                for combos in itertools.combinations(items, n)}
+
+
+def find(items, search, n):
+    return {s for s in items if s.count(search) == n}
+
+
+cards = combine('A23456789TJQK', 'CHDS')
+print(len(cards))
+cards_combos = combo(cards, 2)
+print(random.sample(cards_combos, 5))

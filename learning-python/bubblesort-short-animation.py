bcb76c02877fc979ba830e3657eacba1018b491d
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:11 -0400

    Adjust individual bars to proper height
    Clean up plt objects

--- learning-python/bubblesort-short-animation.py
+++ learning-python/bubblesort-short-animation.py
@@ -2,32 +2,26 @@ import matplotlib.pyplot as plt
 import random
 import numpy as np
 
-plt.style.use(['ggplot'])
-plt.figure(num=None, figsize=(13, 8), dpi=80, facecolor='w', edgecolor='k')
-
 a_list = random.sample(range(50), 50)
 y_pos = np.arange(len(a_list))
-
+plt.title('Bubble Sort Algorithm')
+ax = plt.bar(y_pos, a_list, align='center', color='g', edgecolor='w')
 
 swap = True
 numpasses = len(a_list) - 1
 while numpasses > 0 and swap:
+    plt.pause(0.2)
     swap = False
-    # Need to change individual bars instead of clearing current axes
-    plt.clf()
-    plt.bar(a_list, y_pos, align='center', color='g', alpha=0.5)
+    # Adjust individual bars to proper height of a_list[index]
     for i in range(numpasses):
         if a_list[i] > a_list[i + 1]:
+            ax[i].set_height(a_list[i + 1])
             swap = True
             temp = a_list[i]
             a_list[i] = a_list[i + 1]
             a_list[i + 1] = temp
+            ax[i + 1].set_height(temp)
     numpasses = numpasses-1
-
-    plt.draw()
-    plt.pause(.01)
-    # time.sleep(0.001)
 
 # Window will close without this
 plt.show()
-plt.close()

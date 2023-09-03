8349a2b6d92f7745f3af92b4873a9bcd9eab1dbf
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:13 -0400

    Change self.rank from list to tuple

--- learning-python/deckofcards.py
+++ learning-python/deckofcards.py
@@ -7,7 +7,7 @@ class MyCards(object):
     def __init__(self, counter=0):
         self.value = ('A', '2', '3', '4', '5', '6', '7',
                       '8', '9', '10', 'J', 'Q', 'K')
-        self.rank = ['c', 'd', 'h', 's']
+        self.rank = ('cdhs')
         self.deck = [(A + B) for A in self.value for B in self.rank]
 
     def join(self, iterable, sep=' '):

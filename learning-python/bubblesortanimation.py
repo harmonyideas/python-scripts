bb9a937c09bd9dcb031a4e2696f18940c9f54719
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:11 -0400

    Update individual bars
    Clean up plt objects

--- learning-python/bubblesortanimation.py
+++ learning-python/bubblesortanimation.py
@@ -3,25 +3,22 @@ import random
 import numpy as np
 
 plt.style.use(['ggplot'])
-plt.figure(num=None, figsize=(13, 8), dpi=80, facecolor='w', edgecolor='k')
 a_list = random.sample(range(50), 50)
 y_pos = np.arange(len(a_list))
-
+ax = plt.bar(y_pos, a_list, align='center', color='g', edgecolor='w')
+plt.title('Bubble Sort Algorithm')
 
 for passnum in range(len(a_list), 0, -1):
-    plt.clf()
-    plt.bar(a_list, y_pos, align='center', color='g', alpha=0.5)
-
+    plt.pause(.1)
     for i in range(passnum - 1):
         if a_list[i] > a_list[i + 1]:
+            ax[i].set_height(a_list[i + 1])
             temp = a_list[i]
             a_list[i] = a_list[i + 1]
             a_list[i + 1] = temp
-    plt.draw()
-    plt.pause(.01)
-    # time.sleep(0.001)
-    
+            ax[i + 1].set_height(temp)
+
 # Window will close without this - need to fix and update individual bars instead
 plt.show()
-plt.close()
+
 

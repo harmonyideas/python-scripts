1e41646d2e38fff170f20f30daba0a000b527c53
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:13 -0400

    Add Python3 definition

--- learning-python/pascals-triangle.py
+++ learning-python/pascals-triangle.py
@@ -14,4 +14,24 @@ def pascals_triangle():
             print (coef),
         print ("\n"),
 
-pascals_triangle()
+def pascals_triangle_p3():
+
+    rows = range(0, 10)
+    coef = 1
+
+    for r in rows:
+        for z in range(0, len(rows) - r):
+            print ("", end=" ")
+        for x in range(0, r + 1):
+            if (x == 0 or r == 0):
+                coef = 1
+            else:
+                coef = coef * (r - x + 1) / x
+            print (int(coef), end=" ")
+        print ("\n", end=" ")
+
+# Python2
+#pascals_triangle()
+
+# Python3
+#pascals_triange_p3()

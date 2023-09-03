4a57ed7601a1eba0f35969ebb1ee198a7ffb11fa
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:13 -0400

    Create tweets-timeofday.py

---
+++ learning-python/tweets-timeofday.py
@@ -0,0 +1,31 @@
+import csv
+import matplotlib.pyplot as plt
+from collections import Counter
+import numpy as np
+
+a_list = []
+
+with open('tweets.csv') as csvfile:
+    fieldnames = ['status_id',
+                  'created_at',
+                  'user_id',
+                  'screen_name',
+                  'text']
+
+    stats = csv.DictReader(csvfile, delimiter=',', quotechar='"', fieldnames=fieldnames)
+    stats.next()
+
+    for row in stats:
+        a_list.append((row['created_at'][11:13]))
+
+values = Counter(a_list)
+x_values = values.keys()
+y_values = values.values()
+
+plt.style.use(['ggplot'])
+ax = plt.bar(x_values, y_values, color='r')
+
+plt.title('Tweets by time of day')
+plt.ylabel('Tweets')
+plt.xlabel('Time of day')
+plt.show()

a76a23f8adaf3395094ea357a439313a4f5616b2
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:14 -0400

    Update hashtable.py

--- learning-python/hashtable.py
+++ learning-python/hashtable.py
@@ -4,26 +4,26 @@ class MyHashTable(object):
         self.size = size
 
     def djb2_hash(self, s):
-        hash = 0
+        hash_value = 5381
         for x in str(s):
-            hash = ((hash * 33) ^ ord(x))
-        return hash
+            hash_value = ((hash_value << 5) + hash_value) + ord(x)
+        return hash_value % self.size
 
     def insert(self, key, data):
-        key = (self.djb2_hash(key)) % self.size
-        self.my_table[(key)] = data
+        key_hash = self.djb2_hash(key)
+        self.my_table[key_hash] = data
 
     def getitem(self, key):
-        key = self.djb2_hash(key) % self.size
-        return self.my_table[key]
+        key_hash = self.djb2_hash(key)
+        return self.my_table[key_hash]
 
     def delete(self, key):
-            key = self.djb2_hash(key) % self.size
-            self.my_table[(key)] = 'Empty'
+        key_hash = self.djb2_hash(key)
+        self.my_table[key_hash] = 'Empty'
 
 
 hash_table = MyHashTable(100)
 hash_table.insert('Ping', 'Pong')
-print hash_table.getitem('Ping')
+print(hash_table.getitem('Ping'))
 hash_table.delete('Ping')
-print hash_table.my_table
+print(hash_table.my_table)

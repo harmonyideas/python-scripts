a122e7a5db8b56412033421a37800b319c784f63
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:14 -0400

    Remove plt.pause()

--- learning-python/bubblesortanimation.py
+++ learning-python/bubblesortanimation.py
@@ -8,9 +8,12 @@ y_values = np.arange(len(x_values))
 ax = plt.bar(y_values, x_values, align='center', color='g', edgecolor='w')
 plt.title('Bubble Sort Algorithm')
 
+fig = plt.gcf()
+fig.show()
+
 for numpasses in range(len(x_values), 0, -1):
     for i in range(numpasses - 1):
-        plt.pause(.01)
+        fig.canvas.draw()
         if x_values[i] > x_values[i + 1]:
             ax[i].set_height(x_values[i + 1])
             ax[i + 1].set_height(x_values[i])

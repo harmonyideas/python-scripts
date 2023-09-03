3d9fd96e7da94deec25e9903f48a10d73242c4b7
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:12 -0400

    Test cli commit

--- learning-python/transposematrix.py
+++ learning-python/transposematrix.py
@@ -3,6 +3,5 @@ def transpose_matrix(matrix):
         for j in xrange(i, len(matrix)):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
     return matrix
-            
 
 print(transpose_matrix([[1, 0, 1], [1, 0, 1], [1, 0, 1]]))

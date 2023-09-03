429638af1f10393df8556716efe3ab34aae44e49
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:12 -0400

    Update deckofcards.py

--- learning-python/deckofcards.py
+++ learning-python/deckofcards.py
@@ -1,27 +1,39 @@
 import random
+import itertools
 
 
-class Cards(object):
+class MyCards(object):
     def __init__(self, counter=0):
         self.value = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
         self.rank = ['c', 'd', 'h', 's']
-        Cards.deck = range(52)
-        Cards.deck = [(A + B) for A in self.value for B in self.rank]
+        self.deck = range(52)
+        self.deck = [(A + B) for A in self.value for B in self.rank]
+
+    def join(self, iterable, sep=' '):
+        """Join the items in iterable, mapping each to a string first."""
+        return sep.join(map(str, iterable))
+
+    def card_combinations(self, cards, n):
+        return {self.join(combos)
+                for combos in itertools.combinations(cards, n)}
 
     def pick_random_cards(self, counter):
-        return random.sample(Cards.deck, abs(counter))
+        return random.sample(self.deck, abs(counter))
 
     def shuffle_cards(self, repeat=1):
-        for i in range(0, abs(repeat)):
-            random.shuffle(Cards.deck)
+        for x in range(0, abs(repeat)):
+            random.shuffle(self.deck)
 
     def print_cards(self):
         # for x in range(len(FlashCards.deck)):
-        print Cards.deck
+        print self.deck
 
 
-myCards = Cards()
-myCards.shuffle_cards(1)
-print (myCards.pick_random_cards(4))
+cards = MyCards()
+cards.shuffle_cards(100)
 
-# print myCards.pick_random_cards(4)
+# Give me all combinations for 2 pair of cards
+print (cards.card_combinations(cards.deck, 2))
+
+# Give me 4 random cards
+print (cards.pick_random_cards(4))

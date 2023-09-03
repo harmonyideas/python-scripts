8aca29be640797e74d70eff518367c2611800d65
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:14 -0400

    Update boto3-dynamodb-load.pl

--- learning-python/boto3-dynamodb-load.pl
+++ learning-python/boto3-dynamodb-load.pl
@@ -8,22 +8,23 @@ dynamodb = boto3.resource('dynamodb', region_name='us-east-2', endpoint_url="htt
 table = dynamodb.Table('Legislators')
 
 with open("data.json") as json_file:
-    data = json.load(json_file, parse_float = decimal.Decimal)
+    data = json.load(json_file, parse_float=decimal.Decimal)
 
-    for item in data:
-        code = item['id']
-        name = item['name']['official_full']
-        bio = item['bio']
-        birthday = item['bio']['birthday']
-        terms = item['terms']
+    with table.batch_writer() as batch:
+        for item in data:
+            code = item['id']
+            name = item['name']['official_full']
+            bio = item['bio']
+            birthday = item['bio']['birthday']
+            terms = item['terms']
 
-        print("Adding Legislator:", code, name, bio, birthday, terms)
-        table.put_item(
-         Item={
-               'code': code,
-               'name': name,
-               'bio': bio,
-               'birthday' : birthday,
-               'terms': terms,
-            }
-        )
+            print("Adding Legislator:", code, name, bio, birthday, terms)
+            batch.put_item(
+                Item={
+                    'code': code,
+                    'name': name,
+                    'bio': bio,
+                    'birthday' : birthday,
+                    'terms': terms,
+                }
+            )

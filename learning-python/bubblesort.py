35d3c20e3f0b69c1b2bc033108873b98f9f8f36f
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:23:25 -0400

    Update bubblesort.py

--- learning-python/bubblesort.py
+++ learning-python/bubblesort.py
@@ -8,5 +8,5 @@ def bubble_sort(a_list):
                 a_list[i], a_list[j] = a_list[j], a_list[i]
     return a_list
 
-a_list = random.sample(range(100), 100)
-bubble_sort(a_list)
+my_list = random.sample(range(100), 100)
+bubble_sort(my_list)

c9f5a4b827ec0a10bf78a75dd172780e5020fe18
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:14 -0400

    Update formula

--- learning-python/gauss.py
+++ learning-python/gauss.py
@@ -2,7 +2,7 @@
 # Gauss forumula for finding the sum of a sequence of numbers
 
 def sum_numbers(num_seq):
-    s = num_seq[-1]*(num_seq[0] + num_seq[-1])/2
+    s = num_seq[-1]/2*(num_seq[0] + num_seq[-1])
     return s
 
 num_seq = [i for i in range(1,11)]

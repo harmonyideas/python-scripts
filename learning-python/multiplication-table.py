329b9d8120e11020bbe7372126c02547bb8ae952
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:13 -0400

    Replace rowheaders with iterator

--- learning-python/multiplication-table.py
+++ learning-python/multiplication-table.py
@@ -1,9 +1,7 @@
 def multiplication_table():
 
-    colheaders = ['1', '2', '3', '4', '5',
-                  '6', '7', '8', '9', '10', '11', '12']
-    rowheaders = ['1', '2', '3', '4', '5', 
-                  '6', '7', '8', '9', '10', '11', '12']
+    colheaders = ['1', '2', '3', '4', '5', '6',
+                  '7', '8', '9', '10', '11', '12']
 
     rows = range(1, 13)
     cols = range(1, 13)
@@ -12,10 +10,11 @@ def multiplication_table():
         print("\t" + "[" + c + "]"),
 
     for r in rows:
-        print("\n" + "[" + rowheaders[r - 1] + "]"),
+        print("\n" + "[" + str(r) + "]"),
         for c in cols:
             print ("\t" + str(r * c)),
         print ("\n")
 
 
 multiplication_table()
+

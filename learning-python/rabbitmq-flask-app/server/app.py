a6dc2e7e3e71fc17a724bb7f978661eb0dba24c0
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:14 -0400

    Update app.py
    Update for contactform

--- learning-python/rabbitmq-flask-app/server/app.py
+++ learning-python/rabbitmq-flask-app/server/app.py
@@ -1,33 +1,33 @@
-from flask import Flask
-import pika
+from flask import Flask, request
+import pika, json
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "OK"
+    return 'OK'
 
-@app.route("/create-job/<cmd>")
-def add(cmd):
+@app.route('/contactform', methods=['POST'])
+def contactform():
+    json_data = json.dumps(request.form.to_dict(flat=False))
     try:
         connection = pika.BlockingConnection(pika.ConnectionParameters(host="rabbitmq"))
     except pika.exceptions.AMQPConnectionError as exc:
         print("Failed to connect to RabbitMQ service. Message wont be sent.")
         return
+
     channel = connection.channel()
-    channel.queue_declare(queue="task_queue", durable=True)
+    channel.queue_declare(queue='task_queue', durable=True)
     channel.basic_publish(
-        exchange="",
-        routing_key="task_queue",
-        body=cmd,
+        exchange='',
+        routing_key='task_queue',
+        body=json_data,
         properties=pika.BasicProperties(
             delivery_mode=2,  # make message persistent
-        ),
-    )
+        ))
 
     connection.close()
-    return " ___ Sent: %s" % cmd
+    return " ___ Sent: %s" % json_data
 
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')

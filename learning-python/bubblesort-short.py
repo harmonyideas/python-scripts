78fbc984a64a952d4953bb5ae1727c82b571142c
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:13 -0400

    Update bubblesort-short.py

--- learning-python/bubblesort-short.py
+++ learning-python/bubblesort-short.py
@@ -1,3 +1,6 @@
+import random
+
+
 def bubblesort_short(a_list):
     swap = True
     numpasses = len(a_list) - 1
@@ -8,3 +11,8 @@ def bubblesort_short(a_list):
                 swap = True
                 a_list[i], a_list[i+1] = a_list[i+1], a_list[i]
         numpasses = numpasses - 1
+    return a_list
+
+
+a_list = random.sample(range(100), 100)
+print bubblesort_short(a_list)

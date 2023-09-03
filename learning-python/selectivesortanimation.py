154e6ed62ab8e36ccf9ee9d0ea8f6370c215fcf8
harmonyideas <no-reply@harmonyideas.com>
2023-09-03 15:20:13 -0400

    Remove plt.pause

--- learning-python/selectivesortanimation.py
+++ learning-python/selectivesortanimation.py
@@ -5,11 +5,14 @@ import numpy as np
 plt.style.use(['ggplot'])
 plt.figure(num=None, figsize=(13, 8), dpi=80, facecolor='w', edgecolor='k')
 
+fig = plt.gcf()
+fig.show()
+
 a_list = random.sample(range(100), 100)
 y_pos = np.arange(len(a_list))
 colors = np.random.rand(len(a_list))
 
-for temp_slot in range(len(a_list)-1, 0, -1):
+for temp_slot in range(len(a_list) - 1, 0, -1):
     max_pos = 0
     for position in range(1, temp_slot + 1):
         if a_list[position] > a_list[max_pos]:
@@ -19,11 +22,10 @@ for temp_slot in range(len(a_list)-1, 0, -1):
     a_list[temp_slot] = a_list[max_pos]
     a_list[max_pos] = temp
 
-    # Need to change individual axes instead of clearing the current ones
-    plt.clf()
+#  Clear figure and redraw
+    fig.clf()
     plt.scatter(a_list, y_pos, c=colors, alpha=0.5)
-    plt.pause(0.001)
+    fig.canvas.draw()
 
 # Window will close without this
 plt.show()
-

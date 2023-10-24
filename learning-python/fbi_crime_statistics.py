c6a72a4851653adbd644c6317e54734bdf7d5ed3
harmonyideas <no-reply@harmonyideas.com>
2023-10-24 02:37:25 -0400

    Clean up

--- learning-python/fbi_crime_statistics.py
+++ learning-python/fbi_crime_statistics.py
@@ -3,24 +3,31 @@ import matplotlib.pyplot as plt
 
 # Read CSV file into a DataFrame
 df = pd.read_csv(
-    "static/fbi-stats.csv",
-    delimiter=",",
-    quotechar='"',
-    thousands=",",
-    dtype={"Year": int, "Violent\ncrime": float},
+"static/fbi-stats.csv", 
+delimiter=",", 
+quotechar='"', 
+thousands=",", 
+dtype={"Year": int, "Violent\ncrime": float},
 )
 
-# Extract relevant columns and clean data
+# Select relevant columns and clean data
 df = df[["Year", "Violent\ncrime"]]
-df = df.rename(columns={"Violent\ncrime": "Violent Crime"})
+df.columns = ["Year", "Violent\ncrime"]
 df["Year"] = pd.to_datetime(df["Year"], format="%Y")
-df["Violent Crime"] = df["Violent Crime"].astype(float)
+df["Violent Crime"] = df["Violent\ncrime"].astype(float)
 
-
-# Plot data using pandas built-in functions
+# Plot data using Pandas built-in functions
+plt.figure(figsize=(10, 6))
 plt.style.use(["ggplot"])
-df.plot(x="Year", y="Violent Crime", color="r")
+df.plot(x="Year", y="Violent\ncrime", color="r", linestyle="-", marker="o")
+
+# Customize plot
 plt.title("FBI Violent Crime Statistics")
 plt.ylabel("Volume")
 plt.xlabel("Year")
+plt.xticks(rotation=45)
+plt.grid(True)
+
+# Display plot
 plt.show()
+
